fe1908fb58bc22dffd05432c9b5dd7f9da631c8d
Nijat223 <snijat@icloud.com>
2024-10-08 19:59:49 +0400

    Finished RegisterController
    Created UI Component

---
+++ BillionerApp/Auth/Controllers/RegisterController.swift
@@ -0,0 +1,27 @@
+//
+//  RegisterController.swift
+//  BillionerApp
+//
+//  Created by Nijat Shikhaliyev on 07.10.24.
+//
+
+import UIKit
+
+final class RegisterController: UIViewController{
+    @IBOutlet private weak var submitButton: UIButton!
+    
+    override func viewDidLoad() {
+         super.viewDidLoad()
+         configureButton()
+    }
+    
+    fileprivate func configureButton(){
+        submitButton.addTarget(self, action:#selector(submitButtonClicked), for: .touchUpInside)
+        
+    }
+    @objc
+    fileprivate func submitButtonClicked(){
+        print(#function)
+    }
+}
+
