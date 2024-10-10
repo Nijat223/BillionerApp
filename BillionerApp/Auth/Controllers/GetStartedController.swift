6ce7582dae48309db5f9aad0ac2d6ce19c61c67d
Nijat223 <snijat@icloud.com>
2024-10-10 07:54:34 +0400

    finished LoginController

--- BillionerApp/Auth/Controllers/GetStartedController.swift
+++ BillionerApp/Auth/Controllers/GetStartedController.swift
@@ -11,17 +11,29 @@ final class GetStartedController: UIViewController{
     @IBOutlet private weak var submitButton: UIButton!
     
     override func viewDidLoad() {
-         super.viewDidLoad()
-         configureButton()
+        super.viewDidLoad()
+        configureButton()
     }
     
-    fileprivate func configureButton(){
-        submitButton.addTarget(self, action:#selector(submitButtonClicked), for: .touchUpInside)
+    fileprivate func configureButton() {
+        submitButton.addTarget(self, action: #selector(submitButtonClicked), for: .touchUpInside)
+        submitButton.setTitle("Get Started", for: .normal)
+    }
+    
+    fileprivate func configureUI() {
         
     }
+    
     @objc
-    fileprivate func submitButtonClicked(){
-        print(#function)
+    fileprivate func submitButtonClicked() {
+        showLoginController()
     }
+    
+    fileprivate func showLoginController() {
+        let vc = UIStoryboard.init(name: "Auth", bundle: nil).instantiateViewController(withIdentifier: "LoginController") as? LoginController ?? LoginController()
+        navigationController?.pushViewController(vc, animated: true)
+        
+    }
+    
+    
 }
-
---
+++ BillionerApp/Auth/Models/UserModel.swift
@@ -0,0 +1,16 @@
+//
+//  UserModel.swift
+//  BillionerApp
+//
+//  Created by Nijat Shikhaliyev on 10.10.24.
+//
+
+import Foundation
+
+struct User {
+    let name: String
+    let surname: String
+    let email: String
+    let password: String
+}
+    
